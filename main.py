d6150e6bf917638e69eade4d978525bd63225b5a
DavideRossetto99 <davide.rossetto@student.h-farm.com>
2022-01-23 10:17:49 +0100

    I added help options

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from get_values import create_table
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("crypto")
-parser.add_argument("currency")
-parser.add_argument("-sd","--specific_data")
+parser.add_argument("crypto", help="Specify the cryptocurrency code")
+parser.add_argument("currency", help="Specify the currency code")
+parser.add_argument("-sd","--specific_data", help="Specify which information you want to know")
 args = parser.parse_args()
 print(args.crypto, args.currency, args.specific_data)
 
