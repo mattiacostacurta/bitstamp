5ac3c4b600e3a7b0fc10c3d2b472e45243a03954
calzavarateres <teresa.calzavara@student.h-farm.com>
2022-01-23 11:19:33 +0100

    fixed conflicts

--- get_values.py
+++ get_values.py
@@ -11,3 +11,22 @@ def create_table(value):
     file_json = json.loads(r.text)
     df = pd.DataFrame(file_json, index=[0])
     df.to_csv (r'CryptoTable.csv', index = False, header=True)
+
+
+def get_price():
+    df = pd.read_csv('CryptoTable.csv')
+    last_price = df._get_value(0, 'last')
+    return last_price
+
+
+def get_volume():
+    df = pd.read_csv('CryptoTable.csv')
+    volume = df._get_value(0, 'volume')
+    return volume
+
+
+def get_change():
+    df = pd.read_csv('CryptoTable.csv')
+    open_price = df._get_value(0, 'open')
+    last_price = df._get_value(0, 'last')
+    return (last_price-open_price)/last_price*1004
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from get_values import create_table
+from get_values import create_table, get_price, get_volume, get_change
+
+
 
 create_table(“btcusd”)
