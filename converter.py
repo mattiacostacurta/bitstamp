d05c05d1c50149ced2fa3e0bd46945d0319fff71
DavideRossetto99 <davide.rossetto@student.h-farm.com>
2022-01-23 10:32:08 +0100

    Added convert table function

--- converter.py
+++ converter.py
@@ -8,3 +8,8 @@ def conversion(price, default_currency, output_currency):
    converted_price = c.convert(price, default_currency, output_currency)
    return converted_price
 
+def convert_table(output_currency):
+   df = pd.read_csv('CryptoTable.csv')
+   df['last'] = converter(df._get_value(0, 'last'), "usd", output_currency)
+   df['open'] = converter(df._get_value(0, 'open'), "usd", output_currency)
+   df.to_csv (r'CryptoTable.csv', index = False, header=True)
