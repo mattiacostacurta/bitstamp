66131867964d8562bc557d2ba8dffd3885bb7ede
DavideRossetto99 <davide.rossetto@student.h-farm.com>
2022-01-23 18:05:07 +0100

    Added docstring on get_values function

--- get_values.py
+++ get_values.py
@@ -1,3 +1,9 @@
+'''
+The aim of this module is to retrieve data from the bitstamp API,
+convert it to a csv file, and retrieve values depending on the input
+of the user
+'''
+
 import requests
 import json
 import pandas as pd
@@ -7,6 +13,10 @@ bitstamp_URL = 'https://www.bitstamp.net/api/v2/ticker/%s/'
 
 
 def request_API(value):
+    '''
+    This function requests data from bitstamp API
+    and returns it as a dataframe
+    '''
     r = requests.get(bitstamp_URL % value)
     if r.status_code == 200:
         file_json = json.loads(r.text)
@@ -17,8 +27,9 @@ def request_API(value):
 
 def create_table(df):
     '''
-    This function creates a csv file to fetch 
-    data from bitstamp API and store it in a table
+    This function creates a csv file using the previous dataframe.
+    It checks whether the function receives a dataframe in input, 
+    otherwise it returns False
     '''
     if df is None:
         print('Error: API request unsuccessful')
@@ -38,6 +49,11 @@ def create_table(df):
 
 
 def read_csv(input):
+    '''
+    This function reads CryptoTable.csv file and returns the value
+    received in input by the user.
+    If the input is different from the valid inputs, it returns False
+    '''
     df = pd.read_csv('CryptoTable.csv')
     if input == "last":
         last_price = df._get_value(0, 'last')
